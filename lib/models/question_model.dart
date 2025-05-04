9dd564857628a5928258364d24384e74d5d02c80
mohamed-y <24-mohamedyehia>
2025-05-04 15:54:47 +0300

    Remove category field from Question model

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -2,12 +2,10 @@ class Question {
   final String questionText;
   final List<String> options;
   final int correctAnswerIndex;
-  final String category;
 
   Question({
     required this.questionText,
     required this.options,
     required this.correctAnswerIndex,
-    required this.category,
   });
 }
